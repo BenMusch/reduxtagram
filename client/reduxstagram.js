4ee852af23d90f7223af766664c3c03842d28754
Ben Muschol <benmuschol@gmail.com>
2017-04-20 21:17:18 -0400

    fixed dotfiles and created initial component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react'
+import { render } from 'react-dom'
+import Main from './components/main'
+
+// Import css
+import css from './styles/style.styl'
+
+render(<Main/>, document.getElementById('root'))
